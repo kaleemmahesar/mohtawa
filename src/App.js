28875cb81f703597dc51322854c0dd80c76e2dbe
Kaleem Mahesar <kaleemmahesar@hotmail.com>
2024-05-12 17:22:25 +0500

    after fixing spreadsheet issues UI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
 function App() {
 	useEffect(() => {
-        AOS.init();
+        // AOS.init();
     }, [])
 	return (
 		<div className="site-container">
